2a1049a9b53ac02880af879af18f58bf4708064b
Caivan <caivan2@gmail.com>
2020-09-08 16:12:05 -0500

    Multiple selection Question Structure

--- APPSerPro/src/app/app.module.ts
+++ APPSerPro/src/app/app.module.ts
@@ -10,13 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcfesModulesListComponent } from './serpro-components/icfes-modules-list/icfes-modules-list.component';
 import { ModulesTestListComponent } from './serpro-components/modules-test-list/modules-test-list.component';
 import { IcfesTestComponent } from './serpro-components/modules-test-list/icfes-test/icfes-test.component';
-import { MultipleSelectionQuestionComponent } from './serpro-components/modules-test-list/icfes-test/questions/multiple-selection-question/multiple-selection-question.component';
+import { MultipleSelectionQuestionComponent } from './serpro-components/modules-test-list/questions/multiple-selection-question/multiple-selection-question.component';
 
 // MATERIAL COMPONENTS
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatRadioModule} from '@angular/material/radio';
+
 
 @NgModule({
   declarations: [
