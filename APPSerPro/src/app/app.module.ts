ae1b3308f7c5ddc24e8145a1d64b74d095b16bd7
Caivan <caivan2@gmail.com>
2020-09-08 15:57:12 -0500

    Add material components

--- APPSerPro/src/app/app.module.ts
+++ APPSerPro/src/app/app.module.ts
@@ -1,29 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+
+// PROJECT COMPONENTS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IcfesModulesListComponent } from './serpro-components/icfes-modules-list/icfes-modules-list.component';
 import { ModulesTestListComponent } from './serpro-components/modules-test-list/modules-test-list.component';
 import { IcfesTestComponent } from './serpro-components/modules-test-list/icfes-test/icfes-test.component';
+import { MultipleSelectionQuestionComponent } from './serpro-components/modules-test-list/icfes-test/questions/multiple-selection-question/multiple-selection-question.component';
 
+// MATERIAL COMPONENTS
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
     AppComponent,
     IcfesModulesListComponent,
     ModulesTestListComponent,
-    IcfesTestComponent
+    IcfesTestComponent,
+    MultipleSelectionQuestionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressBarModule,
+    MatRadioModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
