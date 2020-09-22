d2d7e48dd29eb7d6ce00c8e62f2f51b363db9987
Caivan <caivan2@gmail.com>
2020-09-22 01:28:41 -0500

    Fix and organize and complete calls of services

--- APPSerPro/src/app/services/question.service.ts
+++ APPSerPro/src/app/services/question.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from "../../environments/environment";
+import { IcfesModule } from '../models/module.model';
+import { Observable } from 'rxjs';
+import { IcfesTest } from '../models/test.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +12,25 @@ export class QuestionsService {
 
   constructor(private http: HttpClient) { }
 
-  public getId (endpoint: string ,modulo: number){
-    return this.http.get(`${environment.urlQuestionApi}/${endpoint}/${modulo}`);
+  public getAllModules (): Observable<IcfesModule[]>{
+    return this.http.get <IcfesModule[]> (`${environment.urlQuestionApi}/module`);
   }
 
-  public get (endpoint: string){
-    return this.http.get(`${environment.urlQuestionApi}/${endpoint}`);
+  public getTestsByModuleId (moduleId): Observable<IcfesTest[]> {
+    return this.http.get <IcfesTest[]> (`${environment.urlQuestionApi}/icfesTest/${moduleId}`);
+  }
+  
+  public getTestById (testId: string){
+    // TODO: UPDATE WHEN BACK SERVICE IS FIXED
+    //return this.http.get(`${environment.urlQuestionApi}/icfesTest/${testId}`);
+    return this.http.get <IcfesTest[]> (`${environment.urlQuestionApi}/icfesTest/`);
+  }
+
+  public getRandomQuestionsByModuleId (moduleId: string){
+    return this.http.get(`${environment.urlQuestionApi}/question/${moduleId}/${environment.qtyRamdomQuestions}`);
+  }
+
+  public getAllQuestion (endpoint: string){
+    return this.http.get(`${environment.urlQuestionApi}/question`);
   }
 }
