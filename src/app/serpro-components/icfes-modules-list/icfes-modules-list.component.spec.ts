f7a6046b2b05a56aec3f1979eaf620c154cea1a4
caivan <caivan2@gmail.com>
2020-10-06 01:52:34 -0500

    adding dependencies to tests

--- src/app/serpro-components/icfes-modules-list/icfes-modules-list.component.spec.ts
+++ src/app/serpro-components/icfes-modules-list/icfes-modules-list.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IcfesModulesListComponent } from './icfes-modules-list.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
 
 describe('IcfesModulesListComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let component: IcfesModulesListComponent;
   let fixture: ComponentFixture<IcfesModulesListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ IcfesModulesListComponent ]
+      declarations: [ IcfesModulesListComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
--- src/app/serpro-components/modules-test/test-list.component.spec.ts
+++ src/app/serpro-components/modules-test/test-list.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TestListComponent } from './test-list.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 describe('ModulesTestListComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let component: TestListComponent;
   let fixture: ComponentFixture<TestListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestListComponent ]
+      declarations: [ TestListComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   }));
